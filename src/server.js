816590bd951d8cdb5c280a8451d4650694010422
ManarAbed <manarabed333@gmail.com>
2022-02-26 20:46:44 +0200

    updates

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const http = require('http');
+require('env2')('.env');
 const router = require('./router');
 
 const server = http.createServer(router);
 
-const PORT = 8080;
-const HOST = 'localhost';
+const PORT = 8080 || process.env.PORT;
+const HOST = 'localhost' || process.env.HOST;
 
 server.listen(PORT, () => {
   console.log(`the server is listing at http://${HOST}:${PORT}`);
